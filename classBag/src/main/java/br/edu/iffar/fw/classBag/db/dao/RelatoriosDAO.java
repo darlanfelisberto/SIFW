eb101201a42831bc8601523356ab2648b2885220
darlan <darlan.felisberto@iffarroupilha.edu.br>
2024-11-06 11:03:35 -0300

    Closes #2

--- classBag/src/main/java/br/edu/iffar/fw/classBag/db/dao/RelatoriosDAO.java
+++ classBag/src/main/java/br/edu/iffar/fw/classBag/db/dao/RelatoriosDAO.java
@@ -27,7 +27,7 @@ public class RelatoriosDAO extends DAO<Model<?>>{
 //			CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT.withHeader("Nome","Tipo Refeição","Grupo", "Curso"));
 			String and = where.get("where_adicional").toString() + where.get("where_tipo_refeicao");
 			PreparedStatement stm = connection.prepareStatement("""
-					select a.dt_agendamento,u.nome ,tr.descricao, gr.sigla,coalesce(c2.nome,tvs.descricao) as cur_vin from agendamentos a
+					select a.dt_agendamento,p.nome ,tr.descricao, gr.sigla,coalesce(c2.nome,tvs.descricao) as cur_vin from agendamentos a
 					join refeicao r ON r.refeicao_id =a.refeicao_id
 					join tipo_refeicao tr on tr.tipo_refeicao_id =r.tipo_refeicao_id
 					join grupo_refeicoes gr on gr.grupo_refeicoes_id = r.grupo_refeicoes_id 
@@ -37,11 +37,12 @@ public class RelatoriosDAO extends DAO<Model<?>>{
 					left join tipo_vinculo tvS on tvS.tipo_vinculo_id = s.tipo_vinculo_id
 					left join tipo_vinculo tvM on tvM.tipo_vinculo_id = m.tipo_vinculo_id 
 					left join usuarios u ON u.usuario_id = m.usuario_id or u.usuario_id = s.usuario_id 
+                 	left join auth.pessoas p on p.pessoa_id = u.pessoa_id\s
 					where a.credito_id isnull
 					and a.dt_agendamento between ? and ?
 					"""
 					+and+
-					"order by a.dt_agendamento,tr.descricao,4,u.nome");
+					"order by a.dt_agendamento,tr.descricao,4,p.nome");
 			stm.setDate(1, Date.valueOf(dtInicio));
 			stm.setDate(2, Date.valueOf(dtFim));
 
