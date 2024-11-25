f8652948f079dca2e4a4c92297766a39eff68aeb
darlan <darlan.felisberto@iffarroupilha.edu.br>
2024-11-25 10:50:06 -0300

    ajustando geração do baseline e inserts necessarios #15

--- sifw/migrations/V0.0.1.003---index.sql
+++ sifw/migrations/V0.0.1.003---index.sql
@@ -1,2 +1,3 @@
 CREATE INDEX agendamentos_refeicao_id_idx ON public.agendamentos USING btree (refeicao_id, dt_agendamento);
 CREATE INDEX creditos_usuario_id_idx ON public.creditos USING btree (usuario_id);
+
